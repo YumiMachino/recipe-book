66b200d376f7db78d1e9ec102cd792bec45c2d2e
YumiMachino <y.y.1515m@gmail.com>
2021-12-05 16:51:08 -0800

    Add tailwind config for theme color and font family

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,5 +1,5 @@
 const Top = () => {
-  return <div className='container mx-auto bg-gray-200'>Top Page</div>;
+  return <div>Top Page</div>;
 };
 
 export default Top;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#A8763E',
+        secondary: '#F7F3E3',
+        light: '#E8CAA9',
+        dark: '#4F2D07',
+        white: '#fff',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        mulish: ['Mulish', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
