898ffe5ed3027747530ee9e9811072a578931378
YumiMachino <y.y.1515m@gmail.com>
2021-12-07 19:00:55 -0800

    Add responsive on footer

--- src/components/reusable/Footer.tsx
+++ src/components/reusable/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='w-screen h-20 bg-secondary text-primary flex justify-center items-center absolute bottom-0 left-0'>
+    <div className='w-screen h-20 text-sm bg-secondary text-primary absolute bottom-0 left-0 text-center flex justify-center items-center'>
       Copyright &copy; 2021 RecipeBook All Rights Reserved
     </div>
   );
