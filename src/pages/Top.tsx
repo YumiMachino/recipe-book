baef9419382ef6b04571f11b151d9a58ffc25081
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-10 17:32:50 -0800

    add test category for top
    #11
    #16

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,7 +1,18 @@
-import Layout from '../components/reusable/Layout';
+import Category from "../components/reusable/Category";
+import Layout from "../components/reusable/Layout";
+import data from "../data/CategoryData";
 
 const Top = () => {
-  return <Layout>Top page</Layout>;
+  return (
+    <Layout>
+      <div className="m-3 flex">
+        {/* <Category title="Korean\nfood" image="/iamges/category/american.png" /> */}
+        {data.map((item, index) => (
+          <Category key={index} title={item.title} image={item.image} />
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Top;
