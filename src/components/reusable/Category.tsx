3e40ded362038cd748ddc7125c138588b5b0bf0f
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-10 18:07:58 -0800

    fix div to button for category card
    #11

--- src/components/reusable/Category.tsx
+++ src/components/reusable/Category.tsx
@@ -5,12 +5,12 @@ export type CategoryType = {
 
 const Category: React.FC<CategoryType> = ({ title, image }) => {
   return (
-    <div className="relative w-48 h-48">
-      <img className="m-0 object-cover" src={image}/>
+    <button className="relative w-48 h-48 m-3 transform transition duration-500 hover:scale-105">
+      <img className="m-0 object-cover" src={image} />
       <p className="font-main text-white absolute bottom-0 right-2 whitespace-pre-wrap font-semibold text-2xl text-center">
         {title}
       </p>
-    </div>
+    </button>
   );
 };
 
