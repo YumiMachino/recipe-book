b7619a07b01e7338d99b99ba17321d85df4388b1
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-13 16:31:32 -0800

    create hero component
    close #12

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,12 +1,13 @@
 import Category from "../components/reusable/Category";
 import Layout from "../components/reusable/Layout";
 import data from "../data/CategoryData";
+import Hero from "../components/reusable/Hero";
 
 const Top = () => {
   return (
     <Layout>
+      <Hero />
       <div className="m-3 flex">
-        {/* <Category title="Korean\nfood" image="/iamges/category/american.png" /> */}
         {data.map((item, index) => (
           <Category key={index} title={item.title} image={item.image} />
         ))}
