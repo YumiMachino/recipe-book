eef33d1e0a42e19a047b43d8df624eff953b2fdc
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-15 15:07:44 -0800

    create category section on Top page
    #16

--- src/components/reusable/Category.tsx
+++ src/components/reusable/Category.tsx
@@ -5,7 +5,7 @@ export type CategoryType = {
 
 const Category: React.FC<CategoryType> = ({ title, image }) => {
   return (
-    <button className="relative w-48 h-48 m-3 transform transition duration-500 hover:scale-105">
+    <button className="relative w-52 md:w-48 lg:w-62 xl:w-64 m-3 transform transition duration-500 hover:scale-105">
       <img className="m-0 object-cover" src={image} />
       <p className="font-main text-white absolute bottom-0 right-2 whitespace-pre-wrap font-semibold text-2xl text-center">
         {title}
--- src/components/reusable/Hero.tsx
+++ src/components/reusable/Hero.tsx
@@ -10,11 +10,17 @@ const Hero: React.FC = () => {
   return (
     <div className="flex items-center text-center justify-center h-full">
       <img src={Image} width="100%" height="auto" />
-      <div className="absolute mx-96">
-        <div className="flex flex-col gap-10">
-          <MainHeading text="Lorem ipsum dolor sit amet eam quas malorum accusam" />
-          <SubHeading text="Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te." />
-          <div className="flex justify-between mt-16">
+      <div className="absolute mx-10 md:mx-16 lg:mx-52 xl:mx-96">
+        <div className="flex flex-col md:gap-4 lg:gap-10">
+          <MainHeading
+            text="Lorem ipsum dolor sit amet eam quas malorum accusam"
+            color="text-mainHeading"
+          />
+          <SubHeading
+            text="Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te."
+            color="text-subHeading"
+          />
+          <div className="flex justify-between mt-3 lg:mt-12 xl:mt-16">
             <input
               className="grow w-full px-3 mr-5"
               type="text"
