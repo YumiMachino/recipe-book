bbfd085a7503dec6b29584465acd70d40daa1950
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-15 17:02:00 -0800

    create Feartured section
    #16
    モバイルとWebで完全にコンポーネントを出し変えている。
    他にいい方法があれば変更する。

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,12 +1,14 @@
 import Layout from "../components/reusable/Layout";
 import Hero from "../components/reusable/Hero";
 import CategorySection from "../components/template/CategorySection";
+import Featured from "../components/template/Featured";
 
 const Top = () => {
   return (
     <Layout>
       <Hero />
       <CategorySection />
+      <Featured />
     </Layout>
   );
 };
