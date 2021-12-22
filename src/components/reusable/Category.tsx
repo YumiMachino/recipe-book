eac473935e62fc28bdf1038b18dcfa5aeaebeb3f
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2021-12-21 19:19:51 -0800

    create toggle component
    #17
    include prettier formatting

--- src/components/reusable/Category.tsx
+++ src/components/reusable/Category.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export type CategoryType = {
   title: string;
   image: string;
@@ -5,12 +7,14 @@ export type CategoryType = {
 
 const Category: React.FC<CategoryType> = ({ title, image }) => {
   return (
-    <button className="relative w-52 md:w-48 lg:w-62 xl:w-64 m-3 transform transition duration-500 hover:scale-105">
-      <img className="m-0 object-cover" src={image} />
-      <p className="font-main text-white absolute bottom-0 right-2 whitespace-pre-wrap font-semibold text-2xl text-center">
-        {title}
-      </p>
-    </button>
+    <Link to="/results">
+      <button className="relative w-52 md:w-48 lg:w-62 xl:w-64 m-3 transform transition duration-500 hover:scale-105">
+        <img className="m-0 object-cover" src={image} />
+        <p className="font-main text-white absolute bottom-0 right-2 whitespace-pre-wrap font-semibold text-2xl text-center">
+          {title}
+        </p>
+      </button>
+    </Link>
   );
 };
 
